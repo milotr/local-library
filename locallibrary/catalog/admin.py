ec43220f3dd631e8a0a6be76811106f7ffe30cdf
Tran Hoang Minh Long <minhlong010122@gmail.com>
2025-06-05 17:27:07 +0700

    add branch home_page

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
+from .models import Author, Genre, Book, Language, BookInstance
 
-# Register your models here.
+class BookInline(admin.TabularInline):
+    model = Book
+
+@admin.register(Author)
+# admin.site.register(Author, AuthorAdmin)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+    inlines = [BookInline]
+    
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+@admin.register(Book)
+# admin.site.register(Book, BookAdmin)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'display_genre']
+    
+    inlines = [BooksInstanceInline]
+
+@admin.register(BookInstance)
+# admin.site.register(BookInstance, BookInstanceAdmin)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+
+    list_display = ('book', 'status', 'due_back', 'id')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+    
+
+# admin.site.register(Book)
+# admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(Language)
+# admin.site.register(BookInstance)
+
+
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
+from django.http import request
 
-# Create your views here.
+# Create your views here
+def index(request):
+    '''View function for hame page of site.'''
+
+    # generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_authors = Author.objects.count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_authors': num_authors,
+        'num_instances_available': num_instances_available,
+    }
+
+    return render(request, 'index.html', context=context)
